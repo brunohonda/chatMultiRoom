d8bc2ab81ef35e32daa1549f646c0eb6f941e556
Bruno Honda <bruno.honda@4linux.com.br>
2020-04-15 22:14:10 -0300

    Correção de referência de pastas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
