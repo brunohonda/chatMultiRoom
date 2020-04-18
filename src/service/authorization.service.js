ae4251a23a46b17724c591b1db880aa049cc81f2
Bruno Honda <bruno.honda@4linux.com.br>
2020-04-18 11:26:59 -0300

    Retorno de erro específico quando token estiver expirado

--- src/service/authorization.service.js
+++ src/service/authorization.service.js
@@ -5,19 +5,21 @@ module.exports = {
     validate: (req, res, next) => {
         const authorizationHeader = req.header('authorization');
         if (!authorizationHeader) {
-            res.status(401).json({ message: 'Not authorization header found' });
-            return;
+            return res.status(401).json({ message: 'Not authorization header found' });
         }
 
         const token = authorizationHeader.split(' ')[1];
         if (!token) {
-            res.status(401).json({ message: 'Not authorization token found' });
-            return;
+            return res.status(401).json({ message: 'Not authorization token found' });
         }
         try {
             req.jwt = jwt.verify(token, config.jwt.secret);
             next();
         } catch(error) {
+            if (error instanceof jwt.TokenExpiredError) {
+                return res.status(401).json({ message: 'Token expired' });
+            }
+
             res.status(401).json({ message: 'Not authorized' });
         }
     }
